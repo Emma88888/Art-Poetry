ea677a52537cf5a8e13d0e1da2a2f77cdbfc5acc
Emma <langbert95@gmail.com>
2022-06-09 15:28:13 -0400

    deploying

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import { Route, Routes, HashRouter } from "react-router-dom";
+import NewArtwork from './NewArtwork/NewArtwork';
+import Home from "./Home/Home";
+import Gallery from "./Gallery/Gallery";
+import NavBar from "./NavBar/NavBar";
+import { useLocalStorage } from 'react-use'; 
+
+function App() {
+  const [savedArtworks, setSavedArtworks] = useLocalStorage("savedArtworks",[])
+  const addSavedArtwork = (artwork, poem) => {
+    setSavedArtworks([
+      ...savedArtworks,
+      {artwork, poem}
+    ])
+  }
+
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/newArtwork" element={<NewArtwork onSave={addSavedArtwork}/>}/>
+        <Route path="/gallery" element={<Gallery savedArtworks={savedArtworks}/>} />
+      </Routes>
+    </HashRouter>
+  )
+}
+
+export default App;
